3d1ddae5a7edb5a0381b4adfce09d960a077d652
odino <alessandro.nadalin@gmail.com>
2014-06-06 00:47:08 +0400

    added tests and working implementation, gonna remove that eval now...

---
+++ reconfig.js
@@ -0,0 +1,126 @@
+function Reconfig (config) {
+    this.config = config || null;
+}
+
+/**
+ * Replaces overlays in a value of the config.
+ * Overlays are usually references to other config
+ * values such as:
+ *
+ *  helloworld: '{{ hello }} world!'
+ *
+ * so what this function does is basically
+ * resolving the value in the overlay.
+ *
+ * @param {string} value
+ * @param {Config} config
+ * @return {string}
+ */
+Reconfig.prototype.resolveReferences = function(value) {
+    var rcf = this;
+
+    return value.replace(/{{\s*[\w\.]+\s*}}/g, function(parameter){
+        return rcf.get(parameter.replace(/[^\w.]/g, ''));
+    });
+};
+
+/*
+ * Resolves all the parameters of the Config's
+ * value.
+ *
+ * A parameter is a string preceded by a colon,
+ * for example:
+ *
+ *  'This is my config :value'
+ *
+ * This function will be responsible of iterating
+ * through the parameters provided and trying to
+ * replace them in the value. If a parameter is
+ * not found, it will be ignored.
+ *
+ * @param value
+ * @param parameters
+ * @return {*}
+ */
+Reconfig.prototype.resolveParameters = function(value, parameters) {
+    for (var property in parameters) {
+        value = value.replace(':' + property, parameters[property]);
+    }
+
+    return value;
+};
+
+Reconfig.prototype.set =  function (config) {
+    this.config = config;
+}
+
+/**
+ * Returns a configuration value, by path.
+ *
+ * If the configuration contains an object
+ * 'greet' with a property 'formal' of value
+ * 'Hello sir.', you can access it with a
+ * convenient dot notation.
+ *
+ *  Config.get('greet.formal') // Hello sir.
+ *
+ * Values can reference other values, for
+ * example:
+ *
+ * greet: {
+ *   base: 'Hello',
+ *   formal: '{{ greet.base }} sir.'
+ * }
+ *
+ * Values can also contain parameters:
+ *
+ * greet: {
+ *   base: 'Hello',
+ *   formal: '{{ greet.base }} :name.'
+ * }
+ *
+ * which can then be resolved with:
+ *
+ *  Config.get('greet.formal', {name: 'John'})
+ *
+ * which will return "Hello John".
+ *
+ * Please note that this function uses
+ * the 'eval' function, so it should not
+ * be used with not-sanitized user-input.
+ * If you need to retrieve a config value
+ * based on user-input, please sanitize the
+ * value before retrieving it from the
+ * Config object.
+ *
+ * @param path
+ * @param parameters
+ * @return {*}
+ */
+Reconfig.prototype.get = function (path, parameters) {
+    if (!path) {
+        return this.config;
+    }
+
+    try{
+        var value = eval('this.config.' + path);
+
+        if (typeof value === 'string') {
+            value = this.resolveReferences(value);
+
+            if (parameters) {
+                value = this.resolveParameters(value, parameters);
+            }
+        }
+
+        if (value === 'null') {
+            return null;
+        }
+
+        return value || null;
+    } catch(err) {
+        return null;
+    }
+}
+
+module.exports = Reconfig;
---
+++ test/reconfig.js
@@ -0,0 +1,76 @@
+var assert      = require("assert");
+var reconfig    = require("./../reconfig");
+
+describe('Reconfig', function(){
+    describe('new', function(){
+        var config = new reconfig();
+
+        it('should be instantiatable without any parameter', function(){
+            assert('reconfig', typeof config);
+        })
+    })
+
+    describe('get', function(){
+        it('should return NULL if there is no config injected', function(){
+            var config = new reconfig();
+
+            assert(null === config.get());
+        })
+
+        it('should return the whole config object if called without arguments', function(){
+            var values = [1,2,3];
+            var config = new reconfig(values);
+
+            assert(values === config.get());
+        })
+
+        it('should return a specific root config value if called without dots', function(){
+            var values = {a: 1, b: 2};
+            var config = new reconfig(values);
+
+            assert(1 === config.get('a'));
+        })
+
+        it('should return NULL if called with a non existing path', function(){
+            var values = {a: 1, b: 2};
+            var config = new reconfig(values);
+
+            assert(null === config.get('c'));
+        })
+
+        it('should return a deep property if called with the dot notation', function(){
+            var values = {a: {c: 1}, b: 2};
+            var config = new reconfig(values);
+
+            assert(1 === config.get('a.c'));
+        })
+
+        it('should be able to handle parameters', function(){
+            var values = {a: {b: 'hello :what!'}};
+            var config = new reconfig(values);
+
+            assert('hello world!' === config.get('a.b', {what: 'world'}));
+        })
+
+        it('should be able to handle self-referencing configs', function(){
+            var values = {a: {b: 'hello :what!'}, c: "{{ a.b }}"};
+            var config = new reconfig(values);
+
+            assert('hello world!' === config.get('c', {what: 'world'}));
+        })
+
+        it('shouldnt go bonkers if you pass a parameter that doesnt exist', function(){
+            var values = {a: {b: 'hello :what!'}};
+            var config = new reconfig(values);
+
+            assert('hello :what!' === config.get('a.b', {hello: 'world'}));
+        })
+
+        it('should return NULL if self-referencing a parameter that doesnt exist', function(){
+            var values = {a: {b: '{{ c }}'}};
+            var config = new reconfig(values);
+
+            assert(null == config.get('a.b'));
+        })
+    })
+})
